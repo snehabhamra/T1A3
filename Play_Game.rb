c4026d7d56ba0cd569c4d334c59ca1d744e7f2aa
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-05-25 01:04:59 +1000

    added pseudocode to mainmenu file and made some brief updates to play_game file which will have most of the game script to it

--- Play_Game.rb
+++ Play_Game.rb
@@ -1,1 +1,28 @@
-puts "hello from the other side"
+puts "..."
+puts "The air is heavy, and you can hear yourself breathing heavily. You slowly open your eyes, painfully adjusting to the dim light in the unfamiliar surroundings."
+puts "You find yourself in a narrow bedroom, lying on a moldy mattress. A tall wooden door stands before you, clearly bolted shut. From a small window on the wall to your right, you spot a distant crescent moon - it seems to be nighttime."
+puts "A sudden voice echoes in the dark, 'Oh, you're finally up, it's been hours - What's your name?'"
+puts ""
+main_character = gets.chomp 
+puts ""
+    puts "the name #{main_character} escapes your lips before you even register the question. From the corner of your eye you spot a hunched figure on a mattress to your left. It is a girl, barely 14 by the looks of it."
+    puts "'well rise and shine #{main_character}, you've been out for a fair few hours so I thought you were dead', she dully remarks. You study her dishevelled appearance briefly - while she appears young, deep-set scars mar the youthful nature of her face. Her large green eyes seem g and lifeless, and her long auburn hair is matted. Where once may have been a lively and active young girl sat instead a forlorn and hopeless child, who had clearly been through a lot."   
+    puts "Do you ask her name in return? [y/n]:"
+    case (gets.chomp)
+        when "yes"
+        puts "'They call me Sypha'"
+        when "no"
+        puts "You choose to ignore the girl. Instead you look around the decaying room further, trying to understand your surroundings."    
+    end
+    puts "Would you like to a) examine the door, b) inspect the window or c) wander around?"
+    case (gets.chomp)
+    when "c"
+        puts "With a deep breath, you lift yourself up from the mattress with great difficulty, still groggy from the effects of sleep. The floorboards creaking under your weight, you notice that the room is fairly empty lest for two moldy mattresses on opposite ends, a dying oil lamp and a small leatherbound book upon which the lamp rested."
+        puts "Do you reach for the book?"
+        case (gets.chomp)
+            when "yes"
+                puts "You reach for the book"
+            when "no"
+                puts "Interest waning, you return your attention to the room, studying the door instead"
+            end
+    end    
--- mainmenu.rb
+++ mainmenu.rb
@@ -25,30 +25,34 @@ if user_input == 1
     print "> "
     case (gets.chomp)
     when "start"
-        require "../RUBY/Play_Game.rb"
+        require "../T1A3_SBhamra/Play_Game.rb"
     end
 end
 
 if user_input == 2
+    #brief game plot synopsis and explanation.
     puts "The Forgotten is a non-linear, text based mystery game, which involves an amnesiac protagonist who must navigate their way through a mansion of horrors."
     puts "Inspired by older horror games like Resident Evil, Amnesia and the now extinct Choose-Your-Own-Adventure books genre, 'The Forgotten' offers a few different player paths and endings."
     prompt.yes? ("would you like to go back?")
+    #I do not know a more effective way of going back to the main menu prompt itself yet so instead linking back to the ruby file forefront does the trick
     if true
-       require "../RUBY/mainmenu.rb"
+       require "../T1A3_SBhamra/mainmenu.rb"
     end
 end
 
 
 if user_input == 3
+    #begin game prompt, links to another ruby file which contains most of the game script
     puts "Please type 'start' to begin."
     print "> "
     case (gets.chomp)
     when "start"  
-        require "../RUBY/Play_Game.rb"
+        require "../T1A3_SBhamra/Play_Game.rb"
     end
 end
 
 if user_input == 4
+    #quit game prompt, terminates program instead with exit message
     prompt.yes? ("Are you sure you would like to quit?")
     if true 
         puts "Thank you for playing 'The Forgotten: Ruby Edition!'"
