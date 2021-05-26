11695c1a29e3660d4710b6b9d93cf45c49b6ffa7
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-05-26 22:13:12 +1000

    added messages and two paths in gamecontinue.rb that lead to test3.rb file, where user is prompted to go to two different rooms.

--- Play_Game.rb
+++ Play_Game.rb
@@ -1,23 +1,39 @@
 puts "..."
-puts "Blood. Thick blood permeates through the air, and you are suddenly aware of its stench, and along with it your own ragged breathing. You slowly open your eyes, painfully adjusting to the dim light in the unfamiliar surroundings."
-puts "You find yourself in a narrow bedroom, lying on a moldy mattress. A tall wooden door stands before you, clearly bolted shut." 
-puts "On the rightmost wall through a tiny shuttered window, you spot the distant crescent moon - it is nighttime."
-puts ""
-puts "A sudden voice rushes through the dark, 'Oh, you're finally up, I thought you had died', the person coolly remarks"
-puts "..."
+puts "Blood. 
+
+Thick blood permeates through the air, and you are suddenly aware of its overwhelming stench. You slowly open your eyes, painfully adjusting to the dim light in the 
+unfamiliar surroundings."
+puts "
+You find yourself in a narrow bedroom, lying on a moldy mattress. A tall wooden door stands before you, clearly bolted shut." 
+puts "
+On the rightmost wall through a tiny shuttered window, you spot the distant crescent moon - it is nighttime."
+puts "
+A sudden voice rushes through the dark, 'Oh, you're finally up, I thought you had died', the person coolly remarks."
+puts "
+...
+"
 #ok so mc has lost their memory and does not know who sypha is but sypha knows MC, previously sypha asks mc for their name, but to add to mystery we'll make it so sypha knows mc and both are trapped here together. mc needs to figure out their name and identity.
-puts "'Now that you're up, will you tell me your name?', the question hangs in the air, and you take a moment to respond, hesitating to answer"
+puts "'Now that you're up, why don't you tell me your name?', the question hangs in the air, and you take a moment to respond, hesitating to answer."
 main_character = gets.chomp.capitalize 
 puts ""
-    puts "The name #{main_character} escapes your lips. From the corner of your eye you spot a hunched figure on a mattress to your left. It is a girl, barely 14 by the looks of it."
-    puts "'Well rise and shine #{main_character}, you've been out for a fair few hours so I thought you were dead', she dully remarks. You study her dishevelled appearance briefly - while she appears young, deep-set scars mar the youthful nature of her tannedface."  
-    puts "Do you ask her name in return? [y/n]:"
-    case (gets.chomp)
+puts "The name #{main_character} escapes your lips, surprising you.
+
+From the corner of your eye you spot a hunched figure on a mattress to your left. 
+
+It is a girl, barely 14 by the looks of it - she is the one who spoke.
+
+'Well rise and shine #{main_character}, you've been out for a fair few hours so I thought you were dead', she dully remarks. 
+
+You study her dishevelled appearance briefly - while she appears young, deep-set scars mar the youthful nature of her tanned face." 
+puts "
+Do you ask her name in return? [y/n]:"
+case (gets.chomp)
         when "yes"
-        puts "'They call me Sypha'"
+        puts "'You can call me Sypha'"
         require "../T1A3_SBhamra/main.rb"
         when "no"
-        puts "You choose to ignore the girl. Instead you look around the decaying room further, trying to understand your surroundings."
+        puts "You choose to ignore the girl. Instead you look around the decaying room further, trying to understand your surroundings.
+        "
         puts "There does not seem to be much to look at, besides an oil lamp on a book, and the locked door."  
         else 
         puts "..."  
@@ -26,12 +42,16 @@ puts ""
     case (gets.chomp)
         when "yes"
             puts "With a deep breath, you lift yourself up from the mattress with great difficulty, still groggy from the effects of sleep." 
-            puts "The floorboards creaking under your weight, you notice that the room is fairly empty lest for two moldy mattresses on opposite ends, a dying oil lamp and a small leatherbound book upon which the lamp rested."
-            puts "Do you reach for the book? [y/n]"
+            puts "
+            The floorboards creaking under your weight, you notice that the room is fairly empty lest for two moldy mattresses on opposite ends, 
+            a dying oil lamp and a small leatherbound book upon which the lamp rested."
+            puts "
+            Do you reach for the book? [y/n]"
             case (gets.chomp)
             when "yes"
                 puts "You reach for the book; upon inspection it reveals several morbid sketches of corpse-like figures and an ominous figure looming above, dressed all in black.."
-                puts "With fright, you put the book back, and return your attentions to the girl."
+                puts "
+                With fright, you put the book back, and return your attentions to the girl."
                 require "../T1A3_SBhamra/main.rb"
             end
             when "no"
--- gamecontinue.rb
+++ gamecontinue.rb
@@ -10,17 +10,39 @@ choices = [
     {name: "'So how do we escape?'", value: 2},
     ]
 user_input = prompt.select("How should you proceed?", choices) 
+
     if user_input == 1
-        puts "...Please, can you tell me what's going on'?"
-        puts "Haven't I told you? We're now slaves. I've been here two days, and I saw them drag you in 5 hours ago." 
-        puts "Since then I've been hearing what's happening in the other cells. The other kids, they're torturing them."
-        puts "'It will be our turn soon'"
-    end
-    if user_input == 2
-        puts "An irrational fear takes over you. You are alone with this girl, bleeding and trapped in an unfamiliar room." 
-        puts "You have no idea who she is nor can you recall the events of the last 24 hours. In your haste, you decide to attack."
-        puts "Adrenaline overtaking you, you pounce on your target and begin to choke your victim, her arms flailing weakly. A moment of overconfidence as her movements weaken however give her the chance to fight back, and she grabs the oil lamp and smashes it to your head."
-        puts "
+        puts "...Please, can you tell me what's going on'?
         "
-        puts "You are dead, your fear caused you to attack and fail to subdue your target."
+        puts "Haven't I told you? We're now slaves. I've been here two days, and I saw them drag you in 5 hours ago.
+        " 
+        puts "Since then I've been hearing what's happening in the other cells. The other kids, they're torturing them.
+        "
+        puts "'It will be our turn soon, if we don't escape from here.
+        '"
+        puts "You ask her, 'do you know a way out?'
+
+              I do.. I found it earlier when his lackies dragged you in here. You hit your head quite hard and there was some blood. 
+
+              When I was wiping it up I lifted your mattress and found a trapdoor underneath it - it's locked but I think I can open it'.
+              
+              With a little effort Sypha and you both push the mattress and spot the trapdoor underneath. Slowly, you both make your way through.."
+        puts ""
+              sleep (3)
+        require "../T1A3_SBhamra/test3.rb"
+    end
+
+    if user_input == 2
+        puts "You ask her, 'do you know a way out?'
+
+        I do.. I found it earlier when his lackies dragged you in here. You hit your head quite hard and there was some blood. 
+
+        When I was wiping it up I lifted your mattress and found a trapdoor underneath it - it's locked but I think I can open it."
+        puts "She gets up and strides towards my mattress, forcefully pushing it from its location. Below the mattress you see the glint of metal - a handle."
+        puts "
+        You both carefully make your way through the trapdoor, and down a set of stairs which envelope you in darkness.."
+        puts ""
+        puts "******"
+        sleep (3)
+        require "../T1A3_SBhamra/test3.rb"
     end
